3341553e258d7e86425f2218b2c75a156dd12984
Chengkai <ck@qq.com>
2025-05-04 16:48:53 +0800

    批量删除菜品业务代码实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,15 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public interface DishService {
     void saveDish(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteDishByIds(List<Long> ids);
 }
